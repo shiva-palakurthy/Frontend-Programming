7c545070aea83cd40c8576a1c737ff9c160695de
shiva-palakurthy <shivapalakurthy119@gmail.com>
2025-09-05 00:31:30 +0530

    added color to text and added text click commit

--- Mr-Rockline.js
+++ Mr-Rockline.js
@@ -1,5 +1,17 @@
 const button = document.getElementById("Mybutton");
+const Mr_p = document.getElementById("Mr_p")
+
 button.addEventListener("click", function () {
 button.classList.toggle("clicked");
+Mr_p.textContent = "belong to Mr Rockline"
+Mr_p.style.color = '#FFFFFF';
+Mr_p.style.backgroundColor = 'blue';
 });
+
+Mr_p.addEventListener("click" , function(){
+Mr_p.classList.toggle("clicked");
+Mr_p.textContent = "Be carefull it's Me "; 
+Mr_p.style.color = '#FFFFFF';
+Mr_p.style.backgroundColor = 'red' 
+})
 
