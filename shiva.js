21f39d434bf055c8dc683f4b935add9b6b71d771
shiva-palakurthy <shivapalakurthy119@gmail.com>
2025-09-10 16:58:18 +0530

    add the strings in js

---
+++ shiva.js
@@ -0,0 +1,37 @@
+var n = 1;
+var x = 2;
+var sum = n + x;  
+console.log(sum);
+
+//..string 
+var mystr = "hello";
+console.log(mystr);
+
+//.. adding double quoted text in string by using back slash
+var mystr1 = "i am from \"wgl\" nice to meet you";
+console.log(mystr1);
+
+//.. adding double quoted text in string by using single quotes
+var mystr2 = 'hello "welcome" to the party';
+console.log(mystr2);
+
+// writing muilple lines in one string
+// \' single quote , \" double , \\ backslash , \n new line , \r carriage return , \t tab , \b backspace ,\f form feed .
+let mystr3 = 'let take\n "a\rbreak" as we\tare playing from past 45\\mins\b';
+console.log(mystr3);
+
+// adding to text in string
+let  mystr4 = "Mrrockline "+"Shiva";
+console.log(mystr4);
+// adding to sences by using '+='
+let mystr5 = "hello world ";
+mystr5 += "this is wildfire";
+console.log(mystr5);
+// finding the length of the string
+var mystr6 = "hello world";
+mystr6length = mystr6.length;
+console.log(mystr6.length);
+
+
+
+
