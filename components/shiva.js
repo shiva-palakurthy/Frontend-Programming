7bccfdc6271c719beef56f62bed1b03497dc277a
shiva-palakurthy <shivapalakurthy119@gmail.com>
2025-10-02 01:42:58 +0530

    assets-renamed the file names shiva.js - removed unnecessary code, completed todos, made syntax changes page.html - added correct file names

--- components/shiva.js
+++ components/shiva.js
@@ -55,25 +55,24 @@ myarray3[1] = 27;
 console.log(myarray3);
 
 //access muti dimensional array with the Indexes
-var myarray4 =[[1,2] , [3,4] , 2] ; // array with mutiple arrays
+var myarray4 =[[1,2],[3,4],2] ; // array with mutiple arrays
 console.log(myarray4);
-var myarray6 = [[1], [2,3] ,[[7,8], 9]];
-// ! NOTE: you forgot to declare mydata6 with var, let or const
+var myarray6 = [[1], [2,3],[[7,8], 9]];
 var mydata6 = myarray6[2][0][1]; // for geting "8"
 console.log(mydata6);
 // adding the text to an array 
-var myarray7 = ["hello " , "everyone"];
+var myarray7 = ["hello ","everyone"];
 myarray7.push (["to the party"]); //added to the about text
 console.log(myarray7);
 // removing the string from the array
-var myarray8 = ["hello ", "mr " ,"rockline"];
+var myarray8 = ["hello ","mr ","rockline"];
 myarray8.pop(); //pop fuction is only used for the removing of the last element from the array
 console.log(myarray8);
  
 // shift() and unshift()
-var myary = [["shiva " , 1] , ["vishal " , 2]];
+var myary = [["shiva ",1] , ["vishal ",2]];
 myary.shift(); // here the out will be ["vishal " , 2]
-myary.unshift(["vishnu " , 3]);// here ["shiva " 1] will be replaced with ["vishnu " 3]
+myary.unshift(["vishnu ",3]);// here ["shiva " 1] will be replaced with ["vishnu " 3]
 console.log(myary);
 
 // for using text more than one time 
@@ -82,8 +81,6 @@ function reusabletext(){
     console.log("hello world");
 }
 /** 
- * !TODO: Lets discuss variables, operators, conditions, loops, data structures,
- * ! and functions in detail in the next chapter.
  * NOTE: Here we can call the function for n times
    without writing the same text again and again
  * */ 
@@ -91,13 +88,11 @@ const reusableCount = 5;
 for(let i=0; i<reusableCount; i++){
     reusabletext();
 }
-/*
-! Note: No need to write the same text again and again
-reusabletext();// here text will be hello world
-reusabletext();// same repeat here
-reusabletext();// same here we can repeat the text for n times
-*/
-// using function for operations like + - * etc
+/**
+ * getting output of  difference between  @param a and @param b
+ * @param {let} a it is an integer
+ * @param {let} b it is an integer 
+ */ 
 function functionWithArray(a,b) {
     console.log(a-b);// here we can use any of the opration like % * +etc...
 }
